a2bfa26c34eee9c6552a4d64ec26d2f0462438fd
Danick Delaroche <danick.delaroche@lesentames.fr>
2026-02-26 06:24:27 +0100

    créer la page d'erreur

--- src/pages/Erreur/Erreur.jsx
+++ src/pages/Erreur/Erreur.jsx
@@ -1,10 +1,14 @@
 import "../../styles/common.scss";
+import styles from "../../styles/erreur.module.scss";
+import { Link } from "react-router-dom";
 
 function Erreur() {
   return (
-    <>
-      <h1>Page d'erreur</h1>
-    </>
+    <div className={styles.erreurpage}>
+      <div className={styles.erreurpage__404}>404</div>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <Link to="/">Retourner sur la page d'accueil</Link>
+    </div>
   );
 }
 
